76b05d199f8c9c160cdf0c09d2fb6bcb1425a5e9
hsinhuachen <yha0971@gmail.com>
2019-08-17 15:40:37 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,16 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.serve_static_assets = false
+ 
+   # Compress JavaScripts and CSS
+   config.assets.compress = true
+ 
+   # Generate digests for assets URLs
+   config.assets.digest = true
+ 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
